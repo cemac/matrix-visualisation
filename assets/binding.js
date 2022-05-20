cemac/matrix-visualisation

commit 8ab650ff4296ab5a90a4c1e0d24a3a06135648b6

Add key for traffic light level and confidence

assets/binding.js
@@ -2,6 +2,32 @@ var matVisBinding = new Shiny.OutputBinding();
 
 let matvis_vars = $.getJSON( "matvis-1.0.0/matvis_vars.json");
 
+let addLegend = function(node, name, cell_class, levels) {
+
+    let title = document.createElement("h4");
+    title.innerText = name;
+    node.appendChild(title);
+
+    // create legend
+    let table = document.createElement("table");
+    table.className = "matvis-table matvis-table-legend";
+
+    for (let key of Object.keys(levels)) {
+      let row = table.insertRow(-1);
+      let cell = row.insertCell(-1);
+      let class_names = [];
+      class_names.push('matvis-td');
+      class_names.push('matvis-legend-key');
+      class_names.push(cell_class + '-' + levels[key]);
+      cell.className = class_names.join(' ');
+
+      let label_cell = row.insertCell(-1);
+      label_cell.className = 'matvis-legend-label';
+      label_cell.innerText = key;
+    }
+    node.appendChild(table);
+}
+
 $.extend(matVisBinding, {
   find: function(scope) {
     return $(scope).find(".matvis");
@@ -103,6 +129,16 @@ $.extend(matVisBinding, {
         }
       }
     }
+
+    // insert the legend
+    let legend_id = el.id + '-matvis-legend';
+    let legend = document.getElementById(legend_id);
+    let legend_header = document.createElement("h3");
+    legend_header.innerText = 'Key';
+    legend.appendChild(legend_header);
+
+    addLegend(legend, "Co-impact", "matvis-tll", mvars.traffic_light_level);
+    addLegend(legend, "Confidence", "matvis-tlc", mvars.traffic_light_confidence);
   }
 });
 

app.R
@@ -45,6 +45,7 @@ matvisOutput <- function(id) {
     id = id, class = "matvis",
     h1(id = sprintf("%s-matvis-title", id), class = "matvis-title"),
     p(id = sprintf("%s-matvis-desc", id), class = "matvis-desc"),
+    shiny::tags$div(id = sprintf("%s-matvis-legend", id), class = "matvis-legend"),
     shiny::tags$div(id = sprintf("%s-matvis-table", id))
   )
   

assets/styles.css
@@ -13,6 +13,27 @@
   font-size: 3em;
 }
 
+.matvis-legend {
+  float: right;
+}
+
+.matvis-legend-key {
+  line-height: 1.4em;
+  width: 4.5em;
+}
+
+.matvis-legend-key::before {
+  content: "\00a0";
+  font-size: 1.2em;
+  position: relative;
+}
+
+/* Adjust position of symbols */
+.matvis-legend-key.matvis-tlc-1::before { top: 1px; }
+.matvis-legend-key.matvis-tlc-2::before,
+.matvis-legend-key.matvis-tlc-3::before,
+.matvis-legend-key.matvis-tlc-4::before { top: 3px; }
+
 .matvis-table {
   border-spacing: 5px;
   border-collapse: separate;
@@ -79,12 +100,13 @@
 .matvis-table .matvis-tll-3 { background-color: #80cc28; } /* green */
 
 /* Traffic light confidence */
-.matvis-tlc-0::before { content: ""; } /* grey */
+.matvis-tlc-0::before { content: "\00a0"; } /* grey */
 .matvis-tlc-1::before { content: "\25cb"; } /* open circle */
 .matvis-tlc-2::before { content: "\2022"; } /* bullet */
 .matvis-tlc-3::before { content: "\2022 \2022"; } /* 2 bullets */
 .matvis-tlc-4::before { content: "\2022 \2022 \2022"; } /* 3 bullets */
 
+/* Font size adjustments to make symbol sizes match */
 .matvis-tlc-1::before { font-size: 1.2em; }
 .matvis-tlc-2::before,
 .matvis-tlc-3::before,

commit 4857458bc837d833e9131f27a3a122723f0a1a35

Ensure that only single legend is displayed
Fix for bug introduced in commit:8ab650ff with multiple legends being added
when tab panel rendered multiple times.

assets/binding.js
@@ -136,7 +136,7 @@ $.extend(matVisBinding, {
     let legend = document.getElementById(legend_id);
     let legend_header = document.createElement("h3");
     legend_header.innerText = 'Key';
-    legend.appendChild(legend_header);
+    legend.replaceChildren(legend_header);
 
     addLegend(legend, "Co-impact", "matvis-tll", mvars.traffic_light_level);
     addLegend(legend, "Confidence", "matvis-tlc", mvars.traffic_light_confidence);